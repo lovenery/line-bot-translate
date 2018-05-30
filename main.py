75385bbddbc6e5e1c27dacf4ee3b68b022552cab
Chenxu Wang <edittye379@gmail.com>
2018-05-30 20:12:24 +0800

    audio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
-from os import environ
+from os import environ, path
+import requests
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -9,6 +10,7 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
+    AudioSendMessage
 )
 
 app = Flask(__name__)
@@ -37,9 +39,35 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='你說：「'+event.message.text+'」。'))
+    msg = str(event.message.text)
+
+    base_dir = path.abspath(path.dirname(__file__))
+    relative_path = 'static/uploads/'
+    file_name = event.message.id
+    file_extension = '.mp3'
+    file_path = relative_path + file_name + file_extension
+
+    url = 'https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q={}'.format(msg)
+    res = requests.get(url)
+    with open(path.join(base_dir, file_path), 'wb') as fd:
+        for chunk in res.iter_content():
+            fd.write(chunk)
+
+    url_path = path.join('https://' + request.host, file_path)
+    audio_message = AudioSendMessage(
+        original_content_url=url_path,
+        duration=240000,
+    )
+
+    try:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='你說：「'+event.message.text+'」。'),
+            audio_message,
+        )
+    except Exception as e:
+        print('Error:')
+        print(e)
 
 if __name__ == '__main__':
     app.run(port = int(environ['PORT']))
